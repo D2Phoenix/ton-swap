6c6961d6a42b0f221e2af973d4009a59090ec271
Vadim Chadyuk <vadim.chadyuk@gmail.com>
2021-11-16 18:15:25 +0300

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,25 @@ function App() {
       <>
           <div className="header-wrapper">
               <header>
-                  <a href="/" className="logo" />
+                  <div className="container">
+                      <a href="/" className="logo">
+                          <img src="/images/toncoin_white.svg" alt="" />
+                          <span>TONSwap</span>
+                      </a>
+                  </div>
               </header>
               <nav>
                   <div className="nav-wrapper">
-                      <div className="nav-item">
-                          <a className="nav-item__btn">Toncoin</a>
+                      <div className="nav-list">
+                          <div className="nav-item">
+                              <a className="nav-item__btn">Swap</a>
+                          </div>
+                          <div className="nav-item">
+                              <a className="nav-item__btn">Pool</a>
+                          </div>
+                          <div className="nav-item">
+                              <a className="nav-item__btn">Top Pools</a>
+                          </div>
                       </div>
                   </div>
               </nav>
