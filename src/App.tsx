2383182610e923f4930587b8b3bc02552aaca7a0
Vadim Chadyuk <vadim.chadyuk@gmail.com>
2021-11-30 01:19:55 +0300

    Add lazy routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, lazy, Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { Trans, useTranslation } from 'react-i18next';
 
 import './App.scss';
 import Header from './components/Header';
-import SwapPage from './pages/Swap/SwapPage';
-import PoolPage from './pages/Pool/PoolPage';
-import AddLiquidityPage from './pages/AddLiquidity/AddLiquidityPage';
 import { fetchTokens } from './store/app/appThunks';
 import { useAppDispatch, useAppSelector } from './store/hooks';
-import RemoveLiquidityPage from './pages/RemoveLiquidity/RemoveLiquidityPage';
-import PoolsPage from './pages/Pools/PoolsPage';
-import PoolDetailsPage from './pages/PoolDetails/PoolDetailsPage';
-import ImportPoolPage from './pages/ImportPool/ImportPoolPage';
 import { walletCheckTransactions } from './store/wallet/walletThunks';
 import { WALLET_TX_UPDATE_INTERVAL } from './constants/swap';
 import {
@@ -23,6 +16,15 @@ import {
 } from './store/wallet/walletSlice';
 import { Notifications, Notification } from './components/Notifications';
 import { TxStatus, TxType } from './types/transactionInterfaces';
+import Spinner from './components/Spinner';
+
+const SwapPage = lazy(() => import('./pages/Swap/SwapPage'));
+const PoolPage = lazy(() => import('./pages/Pool/PoolPage'));
+const AddLiquidityPage = lazy(() => import('./pages/AddLiquidity/AddLiquidityPage'));
+const RemoveLiquidityPage = lazy(() => import('./pages/RemoveLiquidity/RemoveLiquidityPage'));
+const PoolsPage = lazy(() => import('./pages/Pools/PoolsPage'));
+const PoolDetailsPage = lazy(() => import('./pages/PoolDetails/PoolDetailsPage'));
+const ImportPoolPage = lazy(() => import('./pages/ImportPool/ImportPoolPage'));
 
 function App() {
     const dispatch = useAppDispatch();
@@ -55,23 +57,25 @@ function App() {
           <Header />
           <main>
               <div className="container">
-                  <Routes>
-                      <Route
-                          path="/"
-                          element={<Navigate replace to="/swap" />}
-                      />
-                      <Route path="swap" element={<SwapPage />} />
-                      <Route path="swap/:token0" element={<SwapPage />} />
-                      <Route path="swap/:token0/:token1" element={<SwapPage />} />
-                      <Route path="pool" element={<PoolPage />} />
-                      <Route path="pool/add" element={<AddLiquidityPage />} />
-                      <Route path="pool/add/:token0" element={<AddLiquidityPage />} />
-                      <Route path="pool/add/:token0/:token1" element={<AddLiquidityPage />} />
-                      <Route path="pool/remove/:token0/:token1" element={<RemoveLiquidityPage />} />
-                      <Route path="pool/import" element={<ImportPoolPage />} />
-                      <Route path="pools" element={<PoolsPage />} />
-                      <Route path="pools/:address" element={<PoolDetailsPage />} />
-                  </Routes>
+                  <Suspense fallback={<Spinner />}>
+                      <Routes>
+                              <Route
+                                  path="/"
+                                  element={<Navigate replace to="/swap" />}
+                              />
+                              <Route path="swap" element={<SwapPage />} />
+                              <Route path="swap/:token0" element={<SwapPage />} />
+                              <Route path="swap/:token0/:token1" element={<SwapPage />} />
+                              <Route path="pool" element={<PoolPage />} />
+                              <Route path="pool/add" element={<AddLiquidityPage />} />
+                              <Route path="pool/add/:token0" element={<AddLiquidityPage />} />
+                              <Route path="pool/add/:token0/:token1" element={<AddLiquidityPage />} />
+                              <Route path="pool/remove/:token0/:token1" element={<RemoveLiquidityPage />} />
+                              <Route path="pool/import" element={<ImportPoolPage />} />
+                              <Route path="pools" element={<PoolsPage />} />
+                              <Route path="pools/:address" element={<PoolDetailsPage />} />
+                      </Routes>
+                  </Suspense>
               </div>
               <Notifications>
                   {
