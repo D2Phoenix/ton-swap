c6189559e3c4d962306e507a1104087ae92b899e
Vadim Chadyuk <vadim.chadyuk@gmail.com>
2021-11-29 06:14:01 +0300

    Add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,3 +24,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
+
+serviceWorkerRegistration.register();
